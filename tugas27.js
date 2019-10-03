26b0dc9f23550b83e3452ad415bfa55f168d7d33
Anshor <anshorjihadi@gmail.com>
2019-10-03 13:06:01 +0700

    Tugas 27 Js

---
+++ tugas27.js
@@ -0,0 +1,22 @@
+function satu() {
+  var a = "NO ";
+  var b = "rumah saya ";
+  var c = 30;
+  var d = " A";
+  String(c)
+  var abcd = a + b + c + d
+
+  console.log(abcd);
+}
+satu()
+
+function dua() {
+  var e = "Jalan pangeran Tirtayasa NO ";
+  var f = 70;
+  String(f)
+
+  var ef = e + f;
+  console.log(ef);
+}
+
+dua()
